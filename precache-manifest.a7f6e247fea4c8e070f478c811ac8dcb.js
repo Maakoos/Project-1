be8c4b8586e9325b350eb064ddf2f215fb7784c5
Makos <makos4608@gmail.com>
2020-06-09 15:13:22 +0200

    Updates

---
+++ precache-manifest.a7f6e247fea4c8e070f478c811ac8dcb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4af30d90c755d43633e5d362a5cbd9",
+    "url": "/Project-1/index.html"
+  },
+  {
+    "revision": "8b7009e8ec79beabee9c",
+    "url": "/Project-1/static/js/2.9711d74b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project-1/static/js/2.9711d74b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597de205de1afee7c559",
+    "url": "/Project-1/static/js/main.519e3206.chunk.js"
+  },
+  {
+    "revision": "461a7c702274e71bdf71",
+    "url": "/Project-1/static/js/runtime-main.b113709f.js"
+  },
+  {
+    "revision": "e771805266c69b1df36629a73643d0bf",
+    "url": "/Project-1/static/media/about-us.e7718052.png"
+  },
+  {
+    "revision": "f79e37dc50faeab0a2a58d34c3a92f55",
+    "url": "/Project-1/static/media/achievement-bg.f79e37dc.jpg"
+  },
+  {
+    "revision": "d14c5f29b9370432b9cc6530293d9614",
+    "url": "/Project-1/static/media/banner-bg.d14c5f29.jpg"
+  },
+  {
+    "revision": "abd2b939239ebaa9672b8087b8e93170",
+    "url": "/Project-1/static/media/bannerImage.abd2b939.png"
+  },
+  {
+    "revision": "a9692ffc366c49783879d1d196dc83df",
+    "url": "/Project-1/static/media/choose-plan.a9692ffc.png"
+  },
+  {
+    "revision": "ad94e0e85dfb4d886bb325c1977f3ab6",
+    "url": "/Project-1/static/media/client-1.ad94e0e8.jpg"
+  },
+  {
+    "revision": "4318765a13bc2c51dc0c4708414c2096",
+    "url": "/Project-1/static/media/client-3.4318765a.jpg"
+  },
+  {
+    "revision": "4749a5d7c4d9cd24b107a3cd277e0391",
+    "url": "/Project-1/static/media/footer-bg.4749a5d7.png"
+  }
+]);
